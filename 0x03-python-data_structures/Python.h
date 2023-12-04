f936cec5a71dd52e9b7c27ecd317139e2f0c6086
yomex96 <abayomirobertonawole2@gmail.com>
2023-12-04 08:40:03 -0800

    i don try

---
+++ 0x03-python-data_structures/Python.h
@@ -0,0 +1,22 @@
+#ifndef Py_PYTHON_H
+#define Py_PYTHON_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Various standard C headers */
+
+/* Python object definitions */
+
+/* Typedefs and structures specific to the Python C API */
+
+/* Function declarations for Python C API */
+
+/* Macro definitions for Python C API */
+
+/* ... and more ... */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_PYTHON_H */
