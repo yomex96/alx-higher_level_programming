813bdeb07787146bb436b61deb4481aaab627083
yomex96 <abayomirobertonawole2@gmail.com>
2023-12-06 09:02:11 -0800

    rain

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
+    "returns a key with the biggest integer value"
+
+    biggest_value = 0
+    biggest_key = None
     if a_dictionary:
-        best_sc = list(a_dictionary.keys())[0]
-        for i in a_dictionary.keys():
-            if a_dictionary[i] > a_dictionary.get(best_sc):
-                best_sc = i
-        return best_sc
+        for key, value in a_dictionary.items():
+            if value > biggest_value:
+                biggest_value = value
+                biggest_key = key
+    return biggest_key
