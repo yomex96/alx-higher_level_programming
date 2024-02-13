381cac6bbe0a60d2003df9a843212414a87c0302
yomex96 <abayomirobertonawole2@gmail.com>
2024-02-13 06:36:37 -0800

    waste

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,6 @@
--- A script that updates the score of Bob to 10 in the table second_table.
--- You are not allowed to use Bob’s id value, only the name field
+-- A script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
 -- The database name will be passed as an argument of the mysql command
 
-UPDATE second_table SET score=10 WHERE name="Bob";
+SELECT score, name FROM second_table ORDER BY score DESC;
